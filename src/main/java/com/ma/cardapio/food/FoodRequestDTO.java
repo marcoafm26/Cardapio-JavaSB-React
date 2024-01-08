a4919152fa4d3a05da74712efa713e54eeccab37
Marco Aurélio <marcoaureliofm26@gmail.com>
2024-01-08 08:55:41 -0300

    changes on folder and some increases on application

--- src/main/java/com/ma/cardapio/food/FoodRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package com.ma.cardapio.food;
-
-public record FoodRequestDTO(String title, String image, Integer price) {
-}
--- src/main/resources/db/migration/V1__create-table-foods.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE food(
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    price INTEGER NOT NULL,
-    image TEXT
-)
