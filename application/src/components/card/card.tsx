95a8dd2d58885ab281a30509cc6524e74bd91a1c
Marco Aurélio <marcoaureliofm26@gmail.com>
2024-01-08 10:21:27 -0300

    additions on aplication: api implementation and exibition of cards

--- application/src/components/card/card.tsx
+++ application/src/components/card/card.tsx
@@ -1,17 +1,19 @@
-import './card.css'
+import './card.css';
 
-interface CardProps{
-    price: number,
-    title: string,
-    image: string,
+interface CardProps {
+    price: number;
+    title: string;
+    image: string;
 }
 
-export function Card({price,title,image}: CardProps){
-    return(
-        <div className='card'>
-            <img src="" alt="" />
-            <h2></h2>
-            <p><strong>Valor:</strong></p>
+export function Card({ price, title, image }: CardProps) {
+    return (
+        <div className="card">
+            <img src={image} alt="" />
+            <h2>{title}</h2>
+            <p>
+                <strong>Valor: </strong> {price}
+            </p>
         </div>
-    )
+    );
 }
